dd59cd2e2252f96d9bf1118f560644a9fc7b886c
andrewcwright <acw462@gmail.com>
2013-06-26 10:44:33 -0500

    fix bug from copyed code

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 if Rails.env.development? or Rails.env.test?
-  CapstoneDeadsets::Application.config.secret_token = '45a9cc5f972f0dcd52f663214418ea354e9097f9d83802ab9e2214ba823f179c1a64bee6a636f4f2654140e9323c32cb43e3f47b0371e4332097bda06edcfb7d'
+  BattleNetInformant::Application.config.secret_token = '45a9cc5f972f0dcd52f663214418ea354e9097f9d83802ab9e2214ba823f179c1a64bee6a636f4f2654140e9323c32cb43e3f47b0371e4332097bda06edcfb7d'
 else
-  CapstoneDeadsets::Application.config.secret_token = ENV['SECRET_TOKEN']
+  BattleNetInformant::Application.config.secret_token = ENV['SECRET_TOKEN']
 end
