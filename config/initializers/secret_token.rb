0c9a116c6f4a6f3a9aa0e63828ac7a19f7a35a91
andrewcwright <acw462@gmail.com>
2013-06-26 10:37:17 -0500

    added secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BattleNetInformant::Application.config.secret_token = '45a9cc5f972f0dcd52f663214418ea354e9097f9d83802ab9e2214ba823f179c1a64bee6a636f4f2654140e9323c32cb43e3f47b0371e4332097bda06edcfb7d'
+if Rails.env.development? or Rails.env.test?
+  CapstoneDeadsets::Application.config.secret_token = '45a9cc5f972f0dcd52f663214418ea354e9097f9d83802ab9e2214ba823f179c1a64bee6a636f4f2654140e9323c32cb43e3f47b0371e4332097bda06edcfb7d'
+else
+  CapstoneDeadsets::Application.config.secret_token = ENV['SECRET_TOKEN']
+end
